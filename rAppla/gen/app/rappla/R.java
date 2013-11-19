4663e25b989cb9662d8b6ef3bd873ccd99614b03
Lorenzo Toso <LorToso@gmail.com>
2013-11-19 12:45:11 +0100

    now ignoring the /gen/ folder of the application

--- rAppla/gen/app/rappla/R.java
+++ rAppla/gen/app/rappla/R.java
@@ -10,50 +10,52 @@ package app.rappla;
 public final class R {
     public static final class array {
         public static final int Operations=0x7f050000;
-        public static final int UpdateIntervals=0x7f050001;
+        public static final int UpdateIntervals_Entries=0x7f050001;
+        public static final int UpdateIntervals_Values=0x7f050002;
     }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_action_search_light=0x7f020001;
+        public static final int ic_action_settings_light=0x7f020002;
+        public static final int ic_rappla=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f080007;
-        public static final int editText1=0x7f080004;
-        public static final int editText2=0x7f080005;
+        public static final int TextView01=0x7f080001;
+        public static final int TextView02=0x7f080002;
+        public static final int TextView03=0x7f080003;
+        public static final int TextView04=0x7f080004;
+        public static final int action_refresh=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int fragment_container=0x7f080000;
-        public static final int fragment_settings=0x7f080001;
-        public static final int ratingBar1=0x7f080003;
-        public static final int settings=0x7f080008;
-        public static final int spinner1=0x7f080006;
-        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_rappla=0x7f030000;
-        public static final int activity_settings=0x7f030001;
-        public static final int daylayout=0x7f030002;
-        public static final int fragment_settings=0x7f030003;
-        public static final int trainlayout=0x7f030004;
-        public static final int weeklayout=0x7f030005;
+        public static final int daylayout=0x7f030001;
+        public static final int fragment_settings=0x7f030002;
+        public static final int trainlayout=0x7f030003;
+        public static final int weeklayout=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int DEFAULT_FRAGMENT_TITLE=0x7f040009;
+        public static final int DEFAULT_FRAGMENT_TITLE=0x7f04000a;
         /**  MENÜ 
          */
-        public static final int a=0x7f04000b;
+        public static final int a=0x7f04000c;
         public static final int ans=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int calc=0x7f040002;
         public static final int gmailSync=0x7f040004;
-        public static final int offlineSync=0x7f040008;
+        public static final int offlineSync=0x7f040009;
         public static final int pushNotifications=0x7f040005;
-        public static final int refresh=0x7f04000a;
+        public static final int refresh=0x7f04000b;
         public static final int settings=0x7f040001;
-        public static final int title_activity_settings=0x7f040007;
+        public static final int title_activity_settings=0x7f040008;
         public static final int updateInterval=0x7f040006;
+        public static final int updateInterval_summary=0x7f040007;
     }
     public static final class style {
         /** 
