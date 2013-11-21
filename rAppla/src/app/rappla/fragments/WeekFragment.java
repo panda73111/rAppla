1447c0ed3d238eb6d3f801b78f8fd751faf94d98
Lorenzo Toso <LorToso@gmail.com>
2013-11-21 13:01:45 +0100

    WeekFragment not uses a GridLayout
    And some Dummy events

--- rAppla/src/app/rappla/fragments/WeekFragment.java
+++ rAppla/src/app/rappla/fragments/WeekFragment.java
@@ -1,18 +1,27 @@
 package app.rappla.fragments;
 
-import android.content.SharedPreferences;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import app.rappla.R;
 import app.rappla.StaticContext;
 
-public class WeekFragment extends RapplaFragment implements OnClickListener{
-
+public class WeekFragment extends RapplaFragment implements OnClickListener
+{
+	String gridElementPrefix = "GL#";
+	
+	
+	
 	public WeekFragment()
 	{
 		setTitle(StaticContext.getContext().getResources().getString(R.string.week));
@@ -21,34 +30,70 @@ public class WeekFragment extends RapplaFragment implements OnClickListener{
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.weeklayout, container, false);
-		return rootView;
+        return rootView;
 	}
     public void onStart()
     {
     	super.onStart();
-    	TextView tv1 = (TextView)getActivity().findViewById(R.id.TextView01);
-    	TextView tv2 = (TextView)getActivity().findViewById(R.id.TextView02);
-    	TextView tv3 = (TextView)getActivity().findViewById(R.id.TextView03);
-    	TextView tv4 = (TextView)getActivity().findViewById(R.id.TextView04);
-    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    	configureWeekGrid();
+    	/*SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
     	tv1.setText("Gmail sync is " + sp.getBoolean("gmailSync", false));
     	tv2.setText("Push notifications are " + sp.getBoolean("pushNotifications", false));
     	tv3.setText("Offline sync is " + sp.getBoolean("offlineSync", false));
-    	tv4.setText("updateInterval is " + sp.getString("updateInterval", "0 min"));
+    	tv4.setText("updateInterval is " + sp.getString("updateInterval", "0 min"));*/
     }
     public void onCalcButtonPressed(View view)
     {
     }
     public void onClick(View view)
     {
-    	switch(view.getId())
+    	int itemId = view.getId();
+    	String itemTag = (String)view.getTag();
+    	if(itemTag!=null && itemTag.startsWith(gridElementPrefix))
+    	{
+    		Toast.makeText(getActivity(), (String) view.getTag(), Toast.LENGTH_SHORT).show();
+    	}
+    	switch(itemId)
     	{
 //    	case R.id.action_refresh:
 //    		onRefreshButtonPressed(view);
 //    		break;
     	default:	
     	}
+    }
+    
+    private void configureWeekGrid()
+    {
+    	final int COL_COUNT = 5;
+    	
+        GridLayout weekGrid = (GridLayout)getActivity().findViewById(R.id.weekGrid);
+        weekGrid.setColumnCount(COL_COUNT);
+        weekGrid.setRowCount(40);
+        weekGrid.setUseDefaultMargins(false);
+        weekGrid.setClipToPadding(true);
+
+
+        Point screenSize = getScreenSize();
+        int buttonWidth = screenSize.x/COL_COUNT;
+        for(int i=0; i< 40; i++)
+        {
+        	Button b = new Button(getActivity());
+        	b.setBackgroundResource(R.drawable.unbenannt);
+        	b.setLayoutParams(new LinearLayout.LayoutParams(buttonWidth, buttonWidth));
+        	weekGrid.addView(b);
+        	// X#Y
+        	b.setTag(gridElementPrefix + i%COL_COUNT + "#" + i/COL_COUNT);
+        	b.setOnClickListener(this);
+        }
+    }
+    public Point getScreenSize()
+    {
+    	WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+    	Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size;
     }
 
 }
