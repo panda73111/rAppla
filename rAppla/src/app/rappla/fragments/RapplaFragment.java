35b2761521aa625a102f2dc8c173ea5f6f4d790e
Lorenzo Toso <LorToso@gmail.com>
2013-11-27 14:53:25 +0100

    Fragments can now access the Parsed Calender
    This happens through a Getter method in RapplaFragment

--- rAppla/src/app/rappla/fragments/RapplaFragment.java
+++ rAppla/src/app/rappla/fragments/RapplaFragment.java
@@ -2,7 +2,9 @@ package app.rappla.fragments;
 
 import android.app.Fragment;
 import app.rappla.R;
+import app.rappla.RapplaActivity;
 import app.rappla.StaticContext;
+import app.rappla.calendar.RaplaCalendar;
 
 public abstract class RapplaFragment extends Fragment
 {
@@ -20,5 +22,10 @@ public abstract class RapplaFragment extends Fragment
 	protected void setTitle(String newTitle)
 	{
 		title = newTitle;
+	}
+	
+	protected RaplaCalendar getCalender()
+	{
+		return ((RapplaActivity)getActivity()).getCalender();
 	}
 }
