83fd5a253b6cfa095e69f486e393c983109f28e5
IrtazaS <irtaza@MacbookPro.local>
2014-04-07 13:00:59 +0200

    Mockups to XML
    XML Layout for Alarm, Information & Notes Activity

--- rAppla/gen/app/rappla/R.java
+++ rAppla/gen/app/rappla/R.java
@@ -40,21 +40,36 @@ containing a value of this type.
         public static final int wooden_background_potrait=0x7f020006;
     }
     public static final class id {
-        public static final int action_refresh=0x7f080005;
-        public static final int action_settings=0x7f080006;
-        public static final int layout_day=0x7f080000;
-        public static final int layout_rappla=0x7f080001;
-        public static final int layout_train=0x7f080002;
-        public static final int layout_week=0x7f080003;
-        public static final int pager_week=0x7f080004;
+        public static final int action_refresh=0x7f080011;
+        public static final int action_settings=0x7f080012;
+        public static final int button1=0x7f080000;
+        public static final int button2=0x7f080001;
+        public static final int editText1=0x7f080004;
+        public static final int editText2=0x7f080005;
+        public static final int editText3=0x7f080007;
+        public static final int editText4=0x7f080009;
+        public static final int editText5=0x7f08000b;
+        public static final int layout_day=0x7f080002;
+        public static final int layout_rappla=0x7f08000d;
+        public static final int layout_train=0x7f08000e;
+        public static final int layout_week=0x7f08000f;
+        public static final int pager_week=0x7f080010;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f08000a;
+        public static final int textView5=0x7f08000c;
     }
     public static final class layout {
         public static final int fragment_settings=0x7f030000;
-        public static final int layout_day=0x7f030001;
-        public static final int layout_rappla=0x7f030002;
-        public static final int layout_train=0x7f030003;
-        public static final int layout_week=0x7f030004;
-        public static final int pager_week=0x7f030005;
+        public static final int layout_alarms=0x7f030001;
+        public static final int layout_day=0x7f030002;
+        public static final int layout_information=0x7f030003;
+        public static final int layout_notes=0x7f030004;
+        public static final int layout_rappla=0x7f030005;
+        public static final int layout_train=0x7f030006;
+        public static final int layout_week=0x7f030007;
+        public static final int pager_week=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -66,9 +81,6 @@ containing a value of this type.
         /**  Preferences 
          */
         public static final int gmailSync=0x7f040006;
-        /**  Dummy strings 
-         */
-        public static final int loremIpsum=0x7f04000c;
         public static final int offlineSync=0x7f04000a;
         public static final int pushNotifications=0x7f040007;
         /**  Actionbar 
