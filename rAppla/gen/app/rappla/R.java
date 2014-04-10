6052b0f9cdd575a3e8d1b656d69e323a537d550f
IrtazaS <irtaza@MacbookPro.local>
2014-04-10 11:26:18 +0200

    Sticky Note
    Sticky Note added in layout_notes XML

--- rAppla/gen/app/rappla/R.java
+++ rAppla/gen/app/rappla/R.java
@@ -36,8 +36,9 @@ containing a value of this type.
         public static final int ic_action_refresh=0x7f020002;
         public static final int ic_action_settings_light=0x7f020003;
         public static final int ic_launcher=0x7f020004;
-        public static final int wooden_background_landscape=0x7f020005;
-        public static final int wooden_background_potrait=0x7f020006;
+        public static final int stickynote=0x7f020005;
+        public static final int wooden_background_landscape=0x7f020006;
+        public static final int wooden_background_potrait=0x7f020007;
     }
     public static final class id {
         public static final int action_refresh=0x7f080011;
