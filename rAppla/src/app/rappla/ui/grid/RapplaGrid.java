9fd2c8b4c1fbf61d1d9a4deb128185c9ef146d6d
panda73111 <sebastian.huether@gmx.de>
2014-04-15 12:20:50 +0200

    First draft without GridView and using ViewGroup instead

--- rAppla/src/app/rappla/ui/grid/RapplaGrid.java
+++ rAppla/src/app/rappla/ui/grid/RapplaGrid.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.GridLayout;
+import android.view.ViewGroup;
 import android.widget.Space;
+import app.rappla.R;
 
-public class RapplaGrid extends GridLayout
+public class RapplaGrid extends ViewGroup
 {
 	public final static String elementPrefix = "EP#";
 	public final static String coordinateSeperator = "#";
+
+	private int columnCount, rowCount;
+	private float columnWidth, rowHeight;
 
 	private HashMap<View, RapplaGridElementLayout> allElementLayouts;
 	private ArrayList<View> nonSpaceViews;
@@ -22,9 +26,38 @@ public class RapplaGrid extends GridLayout
 	public RapplaGrid(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
+
 		allElementLayouts = new HashMap<View, RapplaGridElementLayout>();
 		nonSpaceViews = new ArrayList<View>();
-		fillWithSpaces(context);
+
+		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.RapplaGrid, 0, 0);
+
+		try
+		{
+			columnCount = a.getInteger(R.styleable.RapplaGrid_numOfColumns, 1);
+			rowCount = a.getInteger(R.styleable.RapplaGrid_numOfRows, 1);
+		} finally
+		{
+			a.recycle();
+		}
+
+	}
+
+	public int getColumnCount()
+	{
+		return columnCount;
+	}
+
+	public int getRowCount()
+	{
+		return rowCount;
+	}
+
+	private void refreshGridSize(int newWidth, int newHeight)
+	{
+		columnWidth = newWidth / columnCount;
+		rowHeight = newHeight / rowCount;
+		Log.d("RapplaGrid", "refreshGridSize: " + columnWidth + "|" + rowHeight);
 	}
 
 	@Override
@@ -33,7 +66,9 @@ public class RapplaGrid extends GridLayout
 		super.onSizeChanged(w, h, oldw, oldh);
 
 		Log.d("Grid", "Size changed: width " + oldw + " -> " + w + "; height " + oldh + " -> " + h);
-		
+
+		refreshGridSize(w, h);
+
 		for (int i = 0; i < getChildCount(); i++)
 		{
 			View view = getChildAt(i);
@@ -41,9 +76,9 @@ public class RapplaGrid extends GridLayout
 		}
 	}
 
-	public View getElementAt(int x, int y)
+	public View getElementAt(int col, int row)
 	{
-		return findViewWithTag(elementPrefix + x + coordinateSeperator + y);
+		return findViewWithTag(elementPrefix + col + coordinateSeperator + row);
 	}
 
 	public boolean addElement(RapplaGridElement element)
@@ -52,12 +87,12 @@ public class RapplaGrid extends GridLayout
 		return addElementAt(element.getEventButton(), layout.xCoordinate, layout.yCoordinate, layout.rowSpan);
 	}
 
-	public boolean addElementAt(View element, int x, int y)
+	public boolean addElementAt(View element, int col, int row)
 	{
-		return addElementAt(element, x, y, 0);
+		return addElementAt(element, col, row, 0);
 	}
 
-	public boolean addElementAt(View element, int x, int y, int rowSpan)
+	public boolean addElementAt(View element, int col, int row, int rowSpan)
 	{
 		if (allElementLayouts.containsKey(element)) // Sollte das Element schon
 			return false; // hinzugefügt worden sein
@@ -69,24 +104,12 @@ public class RapplaGrid extends GridLayout
 		if (!(element instanceof Space))
 			nonSpaceViews.add(element);
 
-		RapplaGridElementLayout rapplaLayout = new RapplaGridElementLayout(x, y, rowSpan);
+		RapplaGridElementLayout rapplaLayout = new RapplaGridElementLayout(col, row, rowSpan);
 		allElementLayouts.put(element, rapplaLayout);
 		applyLayout(element, rapplaLayout);
 
 		addView(element);
 		return true;
-	}
-
-	private void fillWithSpaces(Context context)
-	{
-		for (int x = 0; x < getColumnCount(); x++)
-		{
-			for (int y = 0; y < getRowCount(); y++)
-			{
-				Space s = new Space(context);
-				addElementAt(s, x, y);
-			}
-		}
 	}
 
 	private void applyLayout(View view, RapplaGridElementLayout layout)
@@ -96,20 +119,45 @@ public class RapplaGrid extends GridLayout
 		applyLayout(view, layout.xCoordinate, layout.yCoordinate, layout.rowSpan);
 	}
 
-	private void applyLayout(View view, int x, int y, int rowSpan)
+	private void applyLayout(View view, int col, int row, int rowSpan)
 	{
+		int newWidth = (int) columnWidth;
+		int newHeight = (int) (rowHeight * rowSpan);
+
 		// Try to get old Layout
-		GridLayout.LayoutParams gParams = (LayoutParams) view.getLayoutParams();
+		ViewGroup.LayoutParams gParams = (LayoutParams) view.getLayoutParams();
 		// If failed, create a new one
 		if (gParams == null)
-			gParams = new GridLayout.LayoutParams();
-
-		gParams.setGravity(Gravity.FILL_VERTICAL);
-		gParams.width = getWidth() / getColumnCount();
-		gParams.height = getHeight() / getRowCount() * rowSpan;
-		gParams.rowSpec = GridLayout.spec(y, rowSpan);
-		gParams.columnSpec = GridLayout.spec(x, 1);
+			gParams = new ViewGroup.LayoutParams(newWidth, newHeight);
+		else
+		{
+			gParams.width = newWidth;
+			gParams.height = newHeight;
+		}
 
 		view.setLayoutParams(gParams);
+		view.setX(col * newWidth);
+		view.setY(row * newHeight);
+	}
+
+	private void refreshLayout(View view)
+	{
+		RapplaGridElementLayout layout = allElementLayouts.get(view);
+		applyLayout(view, layout);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+	{
+		refreshGridSize(getWidth(), getHeight());
+
+		for (int row = 0; row < rowCount; row++)
+			for (int col = 0; col < columnCount; col++)
+			{
+				Log.d("RapplaGrid", "onLayout: " + col + "|" + row);
+				View element = getElementAt(col, row);
+				if (element != null)
+					refreshLayout(element);
+			}
 	}
 }
