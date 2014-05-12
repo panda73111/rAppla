a6b04a5caff3b8e47d7850c02bfe3e6b9fd758fe
IrtazaS <irtaza@MacbookPro.local>
2014-05-12 11:58:33 +0200

    Refactoring: Extract Method 2

--- Patterns/Fowelchapter_1/src/Chapter_1/Customer.java
+++ Patterns/Fowelchapter_1/src/Chapter_1/Customer.java
@@ -31,12 +31,9 @@ class Customer {
 			Rental each = (Rental) enum_rentals.nextElement();
 			// determine amounts for each line
 			thisAmount = each.getCharge();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			
+			frequentRenterPoints += each.getFrequentRenterPoints();
+			
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
 					+ each.getDaysRented() + "\t" + String.valueOf(each.getCharge())
@@ -50,7 +47,7 @@ class Customer {
 				+ " frequent renter points";
 		return result;
 	}
-	
+
 	private double amountFor(Rental aRental) {
 		return aRental.getCharge(); }
 
