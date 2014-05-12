308f76f5f462a2b92329f1c2d1bd52fe2929e075
IrtazaS <irtaza@MacbookPro.local>
2014-05-12 12:19:51 +0200

    Refactoring: Replace Temp with Query 2

--- Patterns/Fowelchapter_1/src/Chapter_1/Customer.java
+++ Patterns/Fowelchapter_1/src/Chapter_1/Customer.java
@@ -31,25 +31,45 @@ class Customer {
 			Rental each = (Rental) enum_rentals.nextElement();
 			// determine amounts for each line
 			thisAmount = each.getCharge();
-			
+
 			frequentRenterPoints += each.getFrequentRenterPoints();
-			
+
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
-					+ each.getDaysRented() + "\t" + String.valueOf(each.getCharge())
-					+ "\n";
-			
+					+ each.getDaysRented() + "\t"
+					+ String.valueOf(each.getCharge()) + "\n";
+
 			totalAmount += each.getCharge();
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
 		return result;
 	}
 
-	private double amountFor(Rental aRental) {
-		return aRental.getCharge(); }
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration enum_rentals = rentals.elements();
+		while (enum_rentals.hasMoreElements()) {
+			Rental each = (Rental) enum_rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
 
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration enum_rentals = rentals.elements();
+		while (enum_rentals.hasMoreElements()) {
+			Rental each = (Rental) enum_rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+	private double amountFor(Rental aRental) {
+		return aRental.getCharge();
+	}
 
 }
