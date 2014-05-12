11508da5b9e615dc0de9a4f9b03daf401d8ea164
IrtazaS <irtaza@MacbookPro.local>
2014-05-12 13:00:54 +0200

    Refactoring: From Template Method

---
+++ Patterns/Fowelchapter_1/src/Chapter_1/Rental.java
@@ -0,0 +1,23 @@
+package Chapter_1;
+
+class Rental {
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	int getFrequentRenterPoints() {
+		return movie.getFrequentRenterPoints(daysRented);
+	}
+}
